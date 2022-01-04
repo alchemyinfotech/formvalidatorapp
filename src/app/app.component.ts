38cb991734512aebf8a2ac5a452a069ecd4febcc
aaa <aaa@gmail.com>
2022-01-04 22:55:16 +0530

    meanformvalidator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formvalidatorapp';
+  angForm:FormGroup;
+  constructor(private fb:FormBuilder)
+  {
+    this.angForm=this.fb.group({company:['',Validators.required]});
+    this.createForm();
+  }
+  createForm()
+  {
+    this.angForm=this.fb.group({name:['',Validators.required],
+    address:['',Validators.required]});
+}
 }
